06888a0477b534948207be5c874a86325e68718a
devrlobs <dev.rommellobiano@gmail.com>
2024-05-08 04:43:14 +0800

    refactor: testing middleware

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using MinimalAPI_Supabase_Postgres;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Supabase;
+using System.Globalization;
+using System.Text;
 using System.Text.Json;
 
 
@@ -17,6 +21,19 @@ builder.Services.AddScoped(provider => new Client(url, key, options));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// builder.Services.AddSession(options => 
+// {
+//     options.Cookie.Name = ".test.Session";
+//     options.IdleTimeout = TimeSpan.FromSeconds(10);
+//     options.Cookie.IsEssential = true;
+// });
+
+// builder.Services.AddControllers().AddNewtonsoftJson(options =>
+//     {
+//         //options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+//         options.SerializerSettings.ContractResolver  =new DefaultContractResolver();
+//     });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,64 +43,103 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/signin", async (Client client, SignInRequest request) =>
+
+
+app.Use(async (context, next) => 
 {
-    await client.Auth.SignInWithPassword(request.Email, request.Password);
-})
-.WithName("signin")
-.WithOpenApi();
+    await context.Response.WriteAsync("Hello 1");
+    await next(context);
+    await context.Response.WriteAsync("Hello 2");
+});
 
-
-app.MapPost("/insertodo", async (TodoRequest request, Client client) =>
+app.MapGet("/", () => 
 {
-    await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
-    Todo todo = new()
-    {
-        Name = request.Name,
-        IsDone = request.IsDone
-    };
-    var res = await client.From<Todo>().Insert(todo);
-    var newTodo = res.Models.First();
-    return Results.Ok(newTodo.Id);
-})
-.WithName("insertodo")
-.WithOpenApi();
+    return "Hello 3";
+});
 
-app.MapGet("/gettodo", async (Client client) =>
+// app.MapPost("/signin", async (Client client, SignInRequest request, ISession session) =>
+// {
+//     //var test = await client.Auth.SignInWithPassword(request.Email, request.Password);
+//     //session.SetString("AuthKey", JsonConvert.SerializeObject(test?.AccessToken) ?? "");
+
+
+//     var auth = client.Auth;
+//     var result = await auth.SignInWithPassword(request.Email, request.Password);
+
+//     var accessToken = result?.AccessToken ?? "";
+//     Cookies.Set("supabase-auth-token", accessToken);
+
+//     return Results.Ok();
+// })
+// .WithName("signin")
+// .WithOpenApi();
+
+
+app.MapGet("/gettodo", async  (Client client) =>
 {
     await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
     var response = await client.From<Todo>().Get();
-    return response.Content;
+    var storage = response.Models;
+    var storage2 = JsonConvert.SerializeObject(storage);
+    return JsonConvert.SerializeObject(response.Models);
 })
 .WithName("gettodo")
 .WithOpenApi();
 
-app.MapPut("/updatetodo/{id}", async (Client client, TodoRequest request, int id) =>
-{
-    await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
 
-    var update = await client
-                    .From<Todo>()
-                    .Where(x => x.Id == id)
-                    .Set(x => x.Name, request.Name).Set(x => x.IsDone, request.IsDone)
-                    .Update();
-    if (update.Models.Count <= 0)
-    {
-        return Results.NotFound("Todo ID supplied not found.");
-    }
-    return Results.Ok("Successfully updated!");
-})
-.WithName("updatetodo")
-.WithOpenApi();
-
-app.MapDelete("/deletetodo/{id}", async (Client client, int id) =>
-{
-    await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
-    await client.From<Todo>().Where(x => x.Id == id).Delete();
-    return Results.Ok("Successfully Deleted!");
-})
-.WithName("deletetodo")
-.WithOpenApi();
 
 
 app.Run();
+
+Client client;
+
+
+// app.MapPost("/signin", async (Client client, SignInRequest request) =>
+// {
+//     await client.Auth.SignInWithPassword(request.Email, request.Password);
+// })
+// .WithName("signin")
+// .WithOpenApi();
+
+
+// app.MapPost("/insertodo", async (TodoRequest request, Client client) =>
+// {
+//     await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
+//     Todo todo = new()
+//     {
+//         Name = request.Name,
+//         IsDone = request.IsDone
+//     };
+//     var res = await client.From<Todo>().Insert(todo);
+//     var newTodo = res.Models.First();
+//     return Results.Ok(newTodo.Id);
+// })
+// .WithName("insertodo")
+// .WithOpenApi();
+
+// app.MapPut("/updatetodo/{id}", async (Client client, TodoRequest request, int id) =>
+// {
+//     await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
+
+//     var update = await client
+//                     .From<Todo>()
+//                     .Where(x => x.Id == id)
+//                     .Set(x => x.Name, request.Name).Set(x => x.IsDone, request.IsDone)
+//                     .Update();
+//     if (update.Models.Count <= 0)
+//     {
+//         return Results.NotFound("Todo ID supplied not found.");
+//     }
+//     return Results.Ok("Successfully updated!");
+// })
+// .WithName("updatetodo")
+// .WithOpenApi();
+
+// app.MapDelete("/deletetodo/{id}", async (Client client, int id) =>
+// {
+//     await client.Auth.SignInWithPassword("testuser@gmail.com", "test1234");
+//     await client.From<Todo>().Where(x => x.Id == id).Delete();
+//     return Results.Ok("Successfully Deleted!");
+// })
+// .WithName("deletetodo")
+// .WithOpenApi();
--- Todo.cs
+++ Todo.cs
@@ -8,10 +8,13 @@ public class Todo : BaseModel
 {
     [PrimaryKey("id")]
     public int Id { get; set; }
+
     [Column("name")]
     public string Name { get; set; } = string.Empty;
+
     [Column("is_done")]
     public bool IsDone { get; set; }
+    
     [Column("created_by")]
     public string CreatedBy {get;set;} = string.Empty;
 }
